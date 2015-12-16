9af5e304e177538e08c433ac9fe23e7fbd65c96f
hayashima1234 <hayashima@dc.tohoku.ac.jp>
2015-12-16 10:55:49 +0900

    Comment

--- ctrl.rb
+++ ctrl.rb
@@ -7,6 +7,8 @@ class TetrisWorldController
   end
 
   def inputted?() return @input_flag end
+  
+  # 操作が成功したかどうかの真偽値を返す
   def rotate_cw()
     return if inputted?
     return @input_flag = @world.rot_cmino(:cw)
@@ -28,21 +30,32 @@ class TetrisWorldController
     return @input_flag = @world.fastfall_cmino()
   end
   
+  # CurrentMinoを返す(core.rb)
   def get_cmino()
     return @world.get_cmino()
   end
+  
+  # TetrisBoardを返す(core.rb)
   def get_board()
     return @world.get_board()
   end
+  
+  # Tetrominoが要素（要素数5）の配列を返す(core.rb)
   def get_nexts()
     return @world.get_nexts()
   end
+  
+  # 消したライン数を変えす
   def get_clrline_num()
     return @world.line_count
   end
+  
+  # cminoの出現数を返す
   def get_spawn_num()
     return @world.spawn_count
   end
+  
+  # ループ回数を返す
   def get_time()
     return @world.past_count
   end
